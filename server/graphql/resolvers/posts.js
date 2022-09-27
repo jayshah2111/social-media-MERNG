3a88a0b6afe6c8e0ea1501485a418a36aad2f0c6
Jay <shahjay2111@gmail.com>
2022-09-27 14:29:10 -0400

    Added middleware for authentication and added functinos for creating and deleting posts

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -1,4 +1,6 @@
-const Post = require('../../models/postModel'); 
+const { AuthenticationError } = require('apollo-server');
+const Post = require('../../models/postModel');
+const checkAuth = require('../utils/check-auth'); 
 
 module.exports = {
     Query: {
@@ -9,6 +11,58 @@ module.exports = {
             } catch(error){
                 throw new Error(error);
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+              const post = await Post.findById(postId);
+              if (post) {
+                return post;
+              } else {
+                throw new Error('Post not found');
+              }
+            } catch (err) {
+              throw new Error(err);
+            }
         }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+            //console.log(user); 
+      
+            if (body.trim() === '') {
+              throw new Error('Post body must not be empty');
+            }
+      
+            const newPost = new Post({
+              body,
+              user: user.id,
+              username: user.username,
+              createdAt: new Date().toISOString()
+            });
+      
+            const post = await newPost.save();
+      
+            // context.pubsub.publish('NEW_POST', {
+            //   newPost: post
+            // });
+      
+            return post;
+        },
+        async deletePost(_, { postId }, context) {
+          const user = checkAuth(context);
+    
+          try {
+            const post = await Post.findById(postId);
+            if (user.username === post.username) {
+              await post.delete();
+              return 'Post deleted successfully';
+            } else {
+              throw new AuthenticationError('Action not allowed');
+            }
+          } catch (err) {
+            throw new Error(err);
+          }
+        },
     }
 }
