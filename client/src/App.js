3f18245ab320aa99f43a55d4af50612e0a41836a
Jay <shahjay2111@gmail.com>
2022-09-28 00:35:16 -0400

    Set up Apollo Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:3000" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
-    <div className="App">
-      lmao
-    </div>
+    <ApolloProvider client={client}>
+      lololol
+    </ApolloProvider>
   );
 }
 
